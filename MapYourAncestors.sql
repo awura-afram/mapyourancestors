d6e2df988a9a78e79386e0cf4851808e92de9d1f
awura-afram <awura.afram@ashesi.edu.gh>
2021-12-11 18:01:58 +0000

    Update MapYourAncestors.sql

--- MapYourAncestors.sql
+++ MapYourAncestors.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `MapYourAncestors`
 --
-
+CREATE DATABASE MapYourAncestors;
 USE MapYourAncestors;
 
 -- --------------------------------------------------------
